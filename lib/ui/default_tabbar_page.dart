d75ec75b80867af375187b2fa48afc9efa91bd0e
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2020-05-30 15:06:20 +0630

    3. At "DefaultTabbarPage", default tabbar and tabbar view are implemented.

--- lib/ui/default_tabbar_page.dart
+++ lib/ui/default_tabbar_page.dart
@@ -3,12 +3,26 @@ import 'package:flutter/material.dart';
 class DefaultTabbarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Default Tabbar Page'),
-      ),
-      body: Center(
-        child: Text('Default Tabbar'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Default Tabbar Page'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.directions_car)),
+              Tab(icon: Icon(Icons.directions_transit)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+          ],
+        ),
       ),
     );
   }
